50829ace942494e83de5976790f88bc85da44201
mahammadaftab <mdaftabeditz360@gmail.com>
2025-08-30 00:45:47 +0530

    updating all files

--- server/users.ts
+++ server/users.ts
@@ -76,3 +76,20 @@ export const signUp = async (email: string, password: string, username: string) 
         }
     }
 }
+
+export const getUsers = async (organizationId: string) => {
+    try {
+        const members = await db.query.member.findMany({
+            where: eq(member.organizationId, organizationId),
+        });
+
+        const users = await db.query.user.findMany({
+            where: not(inArray(user.id, members.map((member) => member.userId))),
+        });
+
+        return users;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
