078c2f73d88a3fc7718ac857b29010b4e234a24a
mahammadaftab <mdaftabeditz360@gmail.com>
2025-10-07 21:36:20 +0530

    updated

--- db/drizzle.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-import { drizzle } from 'drizzle-orm/neon-http';
-import { schema } from './schema';
-
-config({ path: ".env" }); // or .env.local
-
-
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
